afe6b738e6c634904343597305ea0b8c1476b553
dcolon365 <dcolon@microshare.io>
2024-09-08 10:56:24 -0500

    init commit

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace helloworld
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                if(ctx.Request.Method.ToLower() == "get")
+                {
+                    await ctx.Response.WriteAsync("Hello, world!");
+                }
+            });
+        }
+    }
+}
